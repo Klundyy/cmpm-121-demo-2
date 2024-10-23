838fc36b59d46c2cac67f332dc09c3266ab03095
William Klunder <82077867+Klundyy@users.noreply.github.com>
2024-10-23 17:09:27 +0000

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,33 @@ canvasElement.width = 256;
 canvasElement.height = 256;
 canvasElement.id = "canvas";
 app.appendChild(canvasElement);
+
+const ctx = canvasElement.getContext("2d");
+
+let isDrawing = false;
+
+canvasElement.addEventListener("mousedown", () => {
+    isDrawing = true;
+})
+
+canvasElement.addEventListener("mouseup", () => {
+    isDrawing = false;
+    ctx?.beginPath();
+})
+
+canvasElement.addEventListener("mousemove", (pos)=> {
+    if (isDrawing){
+        ctx?.lineTo(pos.offsetX, pos.offsetY);
+        ctx?.stroke();
+        ctx?.beginPath();
+        ctx?.moveTo(pos.offsetX,pos.offsetY);
+    }
+})
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.appendChild(clearButton);
+
+clearButton.addEventListener("click", () => {
+    ctx?.clearRect(0,0, canvasElement.width, canvasElement.height)
+});
