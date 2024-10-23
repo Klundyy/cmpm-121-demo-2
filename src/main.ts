e1535aaa6b5cfbb1702eed2c684489dbe925ede3
William Klunder <82077867+Klundyy@users.noreply.github.com>
2024-10-23 03:51:47 +0000

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const titleHeader = document.createElement("h1");
+titleHeader.textContent = APP_NAME;
+app.appendChild(titleHeader);
+
+const canvasElement = document.createElement("canvas");
+canvasElement.width = 256;
+canvasElement.height = 256;
+canvasElement.id = "canvas";
+app.appendChild(canvasElement);
