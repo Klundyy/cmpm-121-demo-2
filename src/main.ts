fa3d6d7a9740f1b6c0e9460ee9ad28ba2075cc9e
William Klunder <82077867+Klundyy@users.noreply.github.com>
2024-10-25 19:59:57 +0000

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,10 @@ const ctx = canvasElement.getContext("2d");
 
 class markerCommand {
     private points: {x: number, y : number}[] = [];
-    constructor(initX : number, initY : number){
+    private lineWidth: number;
+    constructor(initX : number, initY : number, lineWidth: number){
         this.points.push({x: initX, y: initY});
+        this.lineWidth = lineWidth;
     }
     drag(x: number, y: number){
         this.points.push({x, y});
@@ -28,6 +30,7 @@ class markerCommand {
     display(ctx: CanvasRenderingContext2D){
         if(this.points.length > 0){
             ctx.beginPath();
+            ctx.lineWidth = this.lineWidth;
             ctx.moveTo(this.points[0].x, this.points[0].y);
             for (const position of this.points) {
                 ctx.lineTo(position.x, position.y);
@@ -38,15 +41,18 @@ class markerCommand {
     }
 }
 
+// Line Initialization
+
 let linesList: markerCommand[] = [];
 let redoList: markerCommand[] = [];
 let currentLine: markerCommand | null = null;
+let currentWidth = 1;
 
 let isDrawing = false;
 
 canvasElement.addEventListener("mousedown", (e) => {
     isDrawing = true;
-    currentLine = new markerCommand(e.offsetX, e.offsetY);
+    currentLine = new markerCommand(e.offsetX, e.offsetY, currentWidth);
     redoList = [];
     linesList.push(currentLine);   
 })
@@ -73,9 +79,16 @@ canvasElement.addEventListener("drawing-changed", () => {
     }
 });
 
+// Buttons
+
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear";
 app.appendChild(clearButton);
+clearButton.addEventListener("click", () => {
+    ctx?.clearRect(0,0, canvasElement.width, canvasElement.height)
+    linesList = [];
+    canvasElement.dispatchEvent(new Event("drawing-changed"));
+});
 
 const undoButton = document.createElement("button");
 undoButton.innerHTML = "Undo";
@@ -103,8 +116,21 @@ redoButton.addEventListener("click", () => {
     }
 })
 
-clearButton.addEventListener("click", () => {
-    ctx?.clearRect(0,0, canvasElement.width, canvasElement.height)
-    linesList = [];
-    canvasElement.dispatchEvent(new Event("drawing-changed"));
-});
+const thinButton = document.createElement("button");
+thinButton.innerHTML = "Thin";
+thinButton.classList.add("selectedTool");
+app.appendChild(thinButton);
+thinButton.addEventListener("click", () => {
+    currentWidth = 1;
+    thinButton.classList.add("selectedTool");
+    thickButton.classList.remove("selectedTool");
+})
+
+const thickButton = document.createElement("button");
+thickButton.innerHTML = "Thick";
+app.appendChild(thickButton);
+thickButton.addEventListener("click", () => {
+    currentWidth = 3;
+    thickButton.classList.add("selectedTool");
+    thinButton.classList.remove("selectedTool");
+})
